39a17ddb1beb8ab8c232019e9cbf47a3351381f7
Arham Shajahan <universalmadman@gmail.com>
2024-01-28 19:08:52 +0530

    conversions

---
+++ conversions.js
@@ -0,0 +1,34 @@
+// let score = "90"
+
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// the above line 
+// console.log(typeof valueInNumber);
+
+// one 
+// Nan type = not a Number type
+// when we convert a number "33" => 33
+// but strings like "33b" => NAN.
+
+// other conversions
+
+let isloggedIn =1
+
+let bolleanLoggedIn = Boolean(isloggedIn)
+console.log(bolleanLoggedIn)
+// 1 => True
+// 0 =>false
+
+// Emptystring => False.
+// string => true.
+
+// Conversion to the String.
+
+let someNumber = 78
+
+let stringnumber = String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+// gets converted into the String type.
+
--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 console.log("arham");
 console.log("you");
+
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3); we are using the node js
+
+// readability is very Important in the coding
+
+// Example for the good Readability
+
+console.log(3+3)
+
+console.log(2 +3)
+
+// data types 
+let name = "hitesh"
+let age = 19
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => "" ''
+// boolean => true/false
+// null = standalone value.
+// undefined = we declared the variable but we didn't assign the value
+// symbol => unique
+
+// undefined is a Undefined Type.
+
+//  Object
+
+console.log(typeof (age));
+// 
+// null is a Object
+
+
