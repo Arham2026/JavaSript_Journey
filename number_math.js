1fba0c52b901bb19ff8271b3569d5b47c6b973ad
Arham Shajahan <universalmadman@gmail.com>
2024-02-01 06:19:18 +0530

    this is about maths and numbers

---
+++ number_math.js
@@ -0,0 +1,36 @@
+// when we are using the new String or the num
+// we will be able to see the data type by just logging it
+
+const name = new String("arham")
+
+console.log(name);
+
+// we can change a number to string usse 
+
+// to string() converts to the string and gives some methods that could be used
+// to precision() helps to round off the decimal values
+// to fixed() to fix how many numbers should be there in the decimal place
+
+// tolocalstring() helps us to define the numbers in the US standards
+// use 'en-IN' inside the brack to implement the Indian standard
+
+// --------- MAths--------------------
+
+// Math - it is a Object avaikable in Js 
+
+// Math.abs()
+// Math.round()
+// Math.ciel() takes the top value
+// Math.floor() takes the floor value
+
+// check out the mdn
+
+// Math.random() => throws some random value from 0 to 1
+
+// we could customize it make it throw a random number in between a range.
+
+// first declare 2 var min, max
+
+// then use the formula 
+
+// (Math.random() * (max - min + 1) + min) 
