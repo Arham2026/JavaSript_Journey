b939776781cb1b7fe08b373239020c52d049fdce
Arham Shajahan <universalmadman@gmail.com>
2024-02-07 05:24:59 +0530

    this one is about the symbol declaration

---
+++ objects.js
@@ -0,0 +1,43 @@
+// singleton => when objects are created by constructed
+// Object.create
+
+// object Literals : normal declaration
+
+// Key vlaue pair
+
+
+// declaring the Symbol 
+
+const mysym = Symbol("string")
+
+const user = {
+    name : "arham",
+    age : 19,
+    "full_name" : "arhamshajahan",
+    [mysym] : "key1"
+
+    // using the brackets
+}
+
+// How to access the values
+// two ways 1.dot 2.bracket string
+// console.log(user.age);
+
+// console.log(user["age"]);
+// why we should know both the ways?
+
+// Investigative Study
+
+console.log(user["full_name"]);
+console.log(user[mysym]);
+
+// It is crucially used when the keys are declared in the string format
+
+// how to declare a symbol data type
+
+// by wrapping up with the bracket
+
+// Declare it first with the Symbol()
+
+// [xxxx] : "something" 
+console.log(typeof mysym )
