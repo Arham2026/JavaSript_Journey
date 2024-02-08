8f06f3b0b2e5e80c7da3339118eddf0389b67b14
Arham Shajahan <universalmadman@gmail.com>
2024-02-09 05:29:22 +0530

    This one is About deconstructor and Json

--- objects-02.js
+++ objects-02.js
@@ -38,12 +38,61 @@ console.log(obj3);
 // syntax to go to the empty object.
 
 // we can also use the spread method
-console.log(Object.keys(obj1));
-console.log(Object.values(obj1));
+// console.log(Object.keys(obj1));
+// console.log(Object.values(obj1));
 
 // these both the methods convert the object into the
 // the arrays which will be super helpful
-console.log(Object.entries(obj1));
+// console.log(Object.entries(obj1));
+
+
+// destructuring the objects 
+
+const course = {
+    name : "arham",
+    qualification : "college",
+}
+
+// here if we want to access the objects we have to use 
+// the dot function 
+
+console.log(course.name);
+
+// instead we try to do the below thing
+
+const {name} = course
+
+// now we can access them by just calling it as the name above
+// we no need to use the dot methodology to access it.
+
+// we can also five our own name to call it like
+
+const {name:call} = course
+
+console.log(call);
+
+// just putting the work to others = api
+
+// json
+
+// randomuseer the website for the api 
+
+// note : the Api sends us the message in the json format
+
+// which is similar to the objects (except they have the keys stringed)
+
+// {
+//     "name" : "none",
+//     "route" : "map",
+//     "direction" : "straight"
+    
+// }
+
+// this is how the response from the api looks like
+// it can be objects sometimes it could be array too.
+
+
+
 
 
 
