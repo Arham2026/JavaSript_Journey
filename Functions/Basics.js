b7cd7fc06f74458a611750c6de67f0358304cdfd
Arham Shajahan <universalmadman@gmail.com>
2024-02-10 05:36:30 +0530

    This one is About the collaboration of functions with the Objects

--- Functions/Basics.js
+++ Functions/Basics.js
@@ -1,10 +1,10 @@
 
 // syntax
 function example(){
-    console.log("a");
-    console.log("b");
-    console.log("c");
-    console.log("d");
+    // console.log("a");
+    // console.log("b");
+    // console.log("c");
+    // console.log("d");
 }
 
 // nomenclatures
@@ -27,18 +27,18 @@ function add(a,b){
     return ans;
 }
 
-const addition = console.log("The Result of the function would be ",`${add(10,15)}`);
+// const addition = console.log("The Result of the function would be ",`${add(10,15)}`);
 
 // note for the Interview : if you don't pass an argument then 
         // it is said to be undefined.
 
 function website(username){
     if(!username){
-       console.log('Pass the argument buddy!');
+    //    console.log('Pass the argument buddy!');
         return
     }
     // !username means if username is undefined.('!' not)
-    console.log(`${username} is logged out of Website`);
+    // console.log(`${username} is logged out of Website`);
 }
 
 
@@ -50,16 +50,62 @@ function website(username){
 
 function website(username = "sam"){
     if(!username){
-       console.log('Pass the argument buddy!');
+    //    console.log('Pass the argument buddy!');
         return
     }
     // !username means if username is undefined.('!' not)
-    console.log(`${username} is logged out of Website`);
+    // console.log(`${username} is logged out of Website`);
 }
 
 website()
 
 // you could pass the argument near the parameters.
+
+
+
+
+// rest operator ...
+function calculateCartprize(...number){
+    return number;
+}
+
+console.log(calculateCartprize(56,78,89,900));
+// It gets Converted in to an array.
+
+// Interview : what if ?
+
+function calculateCartprize(num1,num2,...number){
+    return number;
+}
+
+console.log(calculateCartprize(56,78,89,900));
+// the first two argument goes to the first two parameters 
+// the others are passed to the rest Operator.
+
+
+// passing the object in the Object.
+
+function user(anyobject){
+    console.log(anyobject.name)
+    console.log(typeof anyObject);
+    // Undefined bruh!!
+}
+
+console.log(user(
+    {
+        name : "arham",
+        degree : "Iot",
+    }
+));
+
+
+
+
+
+// we can pass the object without making it through the arg 
+ 
+
+// PASSIing the array
 
 
 
