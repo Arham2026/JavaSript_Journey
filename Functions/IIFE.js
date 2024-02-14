4f87e1bb2ad3cc656b2b48bdfad4303b3757e2a7
Arham Shajahan <universalmadman@gmail.com>
2024-02-14 05:38:03 +0530

    This One is About the IIFe

---
+++ Functions/IIFE.js
@@ -0,0 +1,27 @@
+// IIFE(Immdietly Invoking functions Expressions)
+
+function hi(){
+    console.log('hi');
+}
+
+// hi()
+
+
+// why we use it ? => To remove pollution of the global scope
+// How to write the IIfe ()()
+
+// ex: 
+
+(function nothing(){
+    console.log('I have written this code in IIfe');
+})();
+
+// how to write the multiple IIFe in the same page.
+// semi colun is must in the end of the program of the IIFe functions
+
+// unnamed iffi
+( (name) => {
+    console.log(`my name is ${name}`);
+})('kunji');
+
+
